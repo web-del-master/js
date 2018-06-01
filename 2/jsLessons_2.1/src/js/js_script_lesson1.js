7078c879e37c0baad1a6b7878b399e2a3262e138
Сергей Кузнецов <sergey-kuznetsov@inbox.ru>
2018-06-01 20:53:52 +0500

    lesson_2

--- 2/jsLessons_2.1/src/js/js_script_lesson1.js
+++ 2/jsLessons_2.1/src/js/js_script_lesson1.js
@@ -1,7 +1,9 @@
 // Lessons_1.1
+
 // Собираем данные клиента
   var youBudget = prompt('Ваш бюджет на месяц?', '30000');
   var nameStore = prompt('Название вашего магазина?', 'Магазин' );
+
 // сохраняем в обьект содержащий масив.
   var mainList = {
       youBudget,
@@ -11,17 +13,36 @@
       open: false
   };
   
-  var a = 0;
 
+
+for(var a = 0; a < 3; a++){
+  var goods = prompt('Какой тип товаров будем продавать?');
+  if((typeof(goods)) === 'string' && (typeof(goods)) != null && goods != '' && goods.length < 50 ){
+    console.log('Все верно!');
+    mainList.shopGoods[a] = goods;
+  };  
+} ; 
+
+
+// Задание для 2ого урока(2 варианта циклов)
+/*var a = 0;
+do{
+  var goods = prompt('Какой тип товаров будем продавать?');
+  mainList.shopGoods[a] = goods
+  a++
+}while (a <= 2 );*/
+
+/* var a = 0
 while (a < 3){
   var goods = prompt ('Какой тип товаров будем продавать?');
+  a++
   mainList.shopGoods[a] = goods;
-  a++12
-}; 
+}; */
+
 
 //Считаем бюджет на день 
-
   var oneDay = youBudget/30;
   alert('Бюджет на один день '+oneDay);
+
 // Выводим данные в консоль
   console.log(mainList);
---
+++ 2/jsLessons_2.2/src/js/js_script_lesson1.js
@@ -0,0 +1,54 @@
+ var week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+
+ for (var i = 0; i < week.length; i++) {
+      switch (i) {
+      case 0:
+        document.write(week[i] + "<br>");
+        break; 
+      case 1:
+        document.write(week[i] + "<br>");
+        break; 
+      case 2:
+        document.write(week[i] + "<br>");
+        break; 
+      case 3:
+        document.write(week[i] + "<br>");
+        break; 
+      case 4:
+        document.write("<i>" + week[i] + "<br>" + "</i>");
+        break; 
+      case 5:
+        document.write("<strong>" + week[i] + "<br>" + "</strong>");
+        break; 
+      case 6:
+        document.write("<strong>" + week[i] + "<br>" + "</strong>");
+        break;
+      default:
+        // statements_def
+        break;
+    }
+    
+ };
+
+var arr = ["3475", "58789", "784112", "455678", "76452", "61567", "1245123"];
+
+
+for (var b = 0; b < arr.length; b++) {
+      switch (b) {
+      case 0:
+        document.write(arr[b] + "<br>");
+        break; 
+      case 2:
+        document.write(arr[b] + "<br>");
+        break; 
+      case 4:
+        document.write(arr[b] + "<br>");
+        break;       
+      default:
+        // statements_def
+        break;
+    };
+    
+ };
+
+
