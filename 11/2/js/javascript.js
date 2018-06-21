350b318ddf3593398a6a071a1009a627afe77e5d
Сергей Кузнецов <sergey-kuznetsov@inbox.ru>
2018-06-22 00:10:41 +0500

    coment

--- 11/2/js/javascript.js
+++ 11/2/js/javascript.js
@@ -1,8 +1,10 @@
 class Option{
-	constructor(height, width, bg){
+	constructor(height, width, bg, fontSize, textAlign){
 		this.height = height;
 		this.width = width;
-		this.bg = bg;		
+		this.bg = bg;
+		this.fontSize = fontSize;
+		this.textAlign = textAlign;		
 	}
 	createNewDiv(text){
 		let div = document.createElement('div');
@@ -10,11 +12,13 @@ class Option{
 
 		div.style.cssText = `height:${this.height};
 		                     width:${this.width};
-		                     background-color:${this.bg}`;
+		                     background-color:${this.bg};
+		                     font-size:${this.fontSize};
+		                     text-align:${this.textAlign};`
 
 		document.body.appendChild(div); 
 	}
 }
 
-let obj = new Option("200px", "300px", "red");
-obj.createNewDiv("asd")
+let obj = new Option("200px", "300px", "red", "24pt", "center");
+obj.createNewDiv("asd");
