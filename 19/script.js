46c0aa2ca59400e93f25380c27c84894c57c94e0
Сергей Кузнецов <sergey-kuznetsov@inbox.ru>
2018-06-27 13:46:41 +0500

    coment

---
+++ 19/script.js
@@ -0,0 +1,18 @@
+let num = 33721;
+let result = 1;
+let i = num + "";
+    i.trim();
+    
+let sum = i[0]*i[1]*i[2]*i[3]*i[4]
+ 
+console.log(sum);
+
+num = sum 
+
+num = Math.pow(num, 3);
+
+console.log(num);
+
+let a = num + ''
+
+console.log(a[0]+a[1])
